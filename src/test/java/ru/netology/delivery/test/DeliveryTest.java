70e8eb9ab1d95e520a3efceb4a6747da8b921397
IlyaNov <noil.01@mail.ru>
2023-06-21 22:23:23 +0300

    fix two  commit

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -20,13 +20,6 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class DeliveryTest {
-    private static Faker faker;
-
-    @BeforeAll
-    static void setUpAll() {
-        faker = new Faker(new Locale("ru"));
-    }
-
     @BeforeEach
     void setup() {
         open("http://localhost:9999");
